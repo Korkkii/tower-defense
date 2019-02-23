57270e83d033b23efe4e94d4ca3106e65a0acb8b
Henri Korhonen <henri.korhonen@reaktor.com>
2019-02-23 11:27:43 +0200

    Add drawing of board

--- src/main/kotlin/game/Application.kt
+++ src/main/kotlin/game/Application.kt
@@ -10,9 +10,12 @@ import javafx.stage.Stage
 class Main : Application() {
     override fun start(primaryStage: Stage?) {
         primaryStage?.title = "Hello World"
-        val group = GameBoard()
+        val width = 600.0
+        val boardWidth = 400.0
+        val height = 400.0
+        val group = GameBoard(boardWidth, height)
 
-        val scene = Scene(group, 600.0, 400.0, Color.BLUE)
+        val scene = Scene(group, width, height, Color.GRAY)
 
         primaryStage?.scene = scene
         val gameLoop = GameLoop(group)
@@ -21,19 +24,38 @@ class Main : Application() {
     }
 }
 
-class GameBoard : Group(), UpdatableEntity {
-    val rectangle = MovingRectangle(200.0, 100.0, 50.0, 50.0)
+class GameBoard(width: Double, height: Double) : Group(), UpdatableEntity {
+    val board = listOf(
+        listOf(0,1,0,0,0),
+        listOf(0,1,0,0,0),
+        listOf(0,1,1,1,0),
+        listOf(0,0,0,1,0),
+        listOf(0,0,0,1,0))
 
     init {
-        this.children.add(rectangle)
+        val rectangleWidth = width / board[0].size
+        val rectangleHeight = height / board.size
+        val rectangles = board.flatMap { x, y, cellValue ->
+            val color = if (cellValue == 1) Color.BLUE else Color.RED
+            MovingRectangle(x * rectangleWidth, y * rectangleHeight, rectangleWidth, rectangleHeight, color) as Rectangle }
+        this.children.addAll(rectangles)
     }
 
     override fun update() {
-        rectangle.update()
+//        rectangle.update()
     }
 }
 
-class MovingRectangle(x: Double, y: Double, width: Double, height: Double) : Rectangle(x, y, width, height), UpdatableEntity {
+fun <T> List<List<Int>>.flatMap(mappingFunction: (x: Int, y: Int, cellValue: Int) -> T ): List<T> {
+    return this.mapIndexed { columnIndex, cells ->
+        cells.mapIndexed { rowIndex, cellValue ->
+            mappingFunction(rowIndex, columnIndex, cellValue) } }.flatten()
+}
+
+class MovingRectangle(x: Double, y: Double, width: Double, height: Double, color: Color) : Rectangle(x, y, width, height), UpdatableEntity {
+    init {
+        this.fill = color
+    }
     override fun update() {
         this.x += 2
         this.y += 1

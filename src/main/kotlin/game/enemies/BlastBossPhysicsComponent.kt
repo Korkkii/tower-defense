d1c11e4049c1b61dfb915ec594484b1584826031
Henri Korhonen <henri.korhonen@reaktor.com>
2019-06-16 15:22:41 +0200

    Refactor blast physics component functionality into separate modules

--- src/main/kotlin/game/enemies/BlastBossPhysicsComponent.kt
+++ src/main/kotlin/game/enemies/BlastBossPhysicsComponent.kt
@@ -1,22 +1,15 @@
 package game.enemies
 
 import game.AddEntity
-import game.BlindDebuff
 import game.Circle
-import game.DeleteEntity
 import game.GameEntity
 import game.GameState
 import game.PhysicsComponent
-import game.StatusEffect
-import game.StunDebuff
 import game.Vector
-import game.fillCircle
 import game.towers.Tower
-import game.withOpacity
-import javafx.scene.canvas.GraphicsContext
-import javafx.scene.paint.Color
 
-typealias EffectEntityConstructor = (List<Tower>,
+typealias EffectEntityConstructor = (
+    List<Tower>,
     maxRadius: Double,
     position: Vector
 ) -> GameEntity
@@ -47,35 +40,3 @@ class BlastBossPhysicsComponent(
         timeUntilNext = blastCooldown
     }
 }
-
-open class EffectEntity protected constructor(
-    private val towersNear: List<Tower>,
-    private val maxRadius: Double,
-    private val effectConstructor: (Double) -> StatusEffect<Tower>,
-    private val effectColor: Color,
-    position: Vector
-) : GameEntity(position) {
-    private var radius = 1.0
-    private val animationTime = 0.2 // seconds
-    private val growthRate = maxRadius / animationTime
-
-    override fun update(currentState: GameState, delta: Double) {
-        radius += growthRate * delta
-
-        if (radius >= maxRadius) {
-            towersNear.forEach { it.statusEffects += effectConstructor(3.0) }
-            GameState.notify(DeleteEntity(this))
-        }
-    }
-
-    override fun draw(graphics: GraphicsContext, state: GameState) {
-        graphics.fill = effectColor
-        graphics.fillCircle(Circle(position, radius))
-    }
-}
-
-class StunEffect(towersNear: List<Tower>, maxRadius: Double, position: Vector) :
-    EffectEntity(towersNear, maxRadius, ::StunDebuff, Color.ORANGERED, position)
-
-class FlashEffect(towersNear: List<Tower>, maxRadius: Double, position: Vector) :
-    EffectEntity(towersNear, maxRadius, ::BlindDebuff, Color.LIGHTYELLOW.withOpacity(0.4), position)
---
+++ src/main/kotlin/game/enemies/BlastEffectEntity.kt
@@ -0,0 +1,47 @@
+package game.enemies
+
+import game.BlindDebuff
+import game.Circle
+import game.DeleteEntity
+import game.GameEntity
+import game.GameState
+import game.StatusEffect
+import game.StunDebuff
+import game.Vector
+import game.fillCircle
+import game.towers.Tower
+import game.withOpacity
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.paint.Color
+
+open class BlastEffectEntity protected constructor(
+    private val towersNear: List<Tower>,
+    private val maxRadius: Double,
+    private val effectConstructor: (Double) -> StatusEffect<Tower>,
+    private val effectColor: Color,
+    position: Vector
+) : GameEntity(position) {
+    private var radius = 1.0
+    private val animationTime = 0.2 // seconds
+    private val growthRate = maxRadius / animationTime
+
+    override fun update(currentState: GameState, delta: Double) {
+        radius += growthRate * delta
+
+        if (radius >= maxRadius) {
+            towersNear.forEach { it.statusEffects += effectConstructor(3.0) }
+            GameState.notify(DeleteEntity(this))
+        }
+    }
+
+    override fun draw(graphics: GraphicsContext, state: GameState) {
+        graphics.fill = effectColor
+        graphics.fillCircle(Circle(position, radius))
+    }
+}
+
+class StunEffect(towersNear: List<Tower>, maxRadius: Double, position: Vector) :
+    BlastEffectEntity(towersNear, maxRadius, ::StunDebuff, Color.ORANGERED, position)
+
+class FlashEffect(towersNear: List<Tower>, maxRadius: Double, position: Vector) :
+    BlastEffectEntity(towersNear, maxRadius, ::BlindDebuff, Color.LIGHTYELLOW.withOpacity(0.4), position)
